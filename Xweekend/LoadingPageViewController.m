ed5a06e080d003dea215e6d9f1d7f237184e8065
Myth <myth@MythMBP.local>
2013-05-03 12:43:51 +0800

    最终提交版本

--- Xweekend/LoadingPageViewController.m
+++ Xweekend/LoadingPageViewController.m
@@ -73,68 +73,74 @@
 {
 //            //    NSLog(@"superView = %@",self.view.superview);
     self.view.frame = CGRectMake(0, 0, 768, 1024);
+    
+    
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"isLogined"]) {
+//        [UIView beginAnimations: nil context: nil];
+//        [UIView setAnimationDuration: 0.5f];
+//        [UIView setAnimationDidStopSelector:@selector(finishedFading)];
+//        startLoge.alpha = 0.0;
+//        [UIView commitAnimations];
+        
+        [self showMain];
+    }else{
+        NSArray *arrImage = [[NSArray alloc]initWithObjects:@"guide1",@"guide2",@"guide3",@"guide4",@"guide5", nil];
+        if (guideView) {
+            //        [guideView release];
+        }
+        guideView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, 768, 1024)];
+        guideView.pagingEnabled = YES;
+        guideView.contentSize = CGSizeMake(self.view.frame.size.width*5, 1024);
+        guideView.showsHorizontalScrollIndicator = NO;
+        guideView.showsVerticalScrollIndicator = NO;
+        //    guideView.scrollsToTop = NO;
+        guideView.delegate = self;
+        //    guideView.minimumZoomScale = .25;
+        //    guideView.maximumZoomScale = 20;
+        //    guideView.canCancelContentTouches = YES;
+        //    guideView.userInteractionEnabled = YES;
+        //    guideView.alwaysBounceVertical = YES;
+        
+        pageControl = [[UIPageControl alloc]initWithFrame:CGRectMake(0, 1024-36, 768, 36)];
+        pageControl.numberOfPages=[arrImage count];
+        //    [pageControl setCurrentPageIndicatorTintColor:[UIColor yellowColor]];
+        
+        for ( int i = 0; i<[arrImage count]; i++) {
+            //        NSString *url=[arr objectAtIndex:i];
+            //        UIButton *img=[[UIButton alloc]initWithFrame:CGRectMake(320*i, 0, 320, 189)];
+            NSString *path = [[NSBundle mainBundle] pathForResource:[arrImage objectAtIndex:i] ofType:@"jpg"];
+            UIImage *image = [UIImage imageWithContentsOfFile:path];
+            //        NSLog(@"%f",image.size.width);
+            UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+            imageView.frame = CGRectMake(768*i, 0, 768, 1024);
+            [guideView addSubview:imageView];
+            [imageView release];
+        }
+        [arrImage release];
+        [self.view addSubview:guideView];
+        [self.view addSubview:pageControl];
+        [self.view bringSubviewToFront:startLoge];
+        //	[UIView beginAnimations: nil context: nil];
+        //	[UIView setAnimationDuration: 0.3f];
+        //	[UIView setAnimationDelegate: self];
+        //	[UIView setAnimationDidStopSelector: @selector(finishedFading)];
+        //	startLoge.alpha = 1.0;
+        //	[UIView commitAnimations];
+        
+        [UIView beginAnimations: nil context: nil];
+        [UIView setAnimationDuration: 0.5f];
+        [UIView setAnimationDelegate: self];
+        startLoge.alpha = 0.0;
+        [UIView setAnimationDidStopSelector:@selector(finishedFading)];
+        [UIView commitAnimations];
+    }
+    
+    
 
-    NSArray *arrImage = [[NSArray alloc]initWithObjects:@"guide1",@"guide2",@"guide3",@"guide4",@"guide5", nil];
-    if (guideView) {
-//        [guideView release];
-    }
-    guideView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, 768, 1024)];
-    guideView.pagingEnabled = YES;
-    guideView.contentSize = CGSizeMake(self.view.frame.size.width*5, 1024);
-    guideView.showsHorizontalScrollIndicator = NO;
-    guideView.showsVerticalScrollIndicator = NO;
-//    guideView.scrollsToTop = NO;
-    guideView.delegate = self;
-//    guideView.minimumZoomScale = .25;
-//    guideView.maximumZoomScale = 20;
-//    guideView.canCancelContentTouches = YES;
-//    guideView.userInteractionEnabled = YES;
-//    guideView.alwaysBounceVertical = YES;
-    
-    pageControl = [[UIPageControl alloc]initWithFrame:CGRectMake(0, 1024-36, 768, 36)];
-    pageControl.numberOfPages=[arrImage count];
-//    [pageControl setCurrentPageIndicatorTintColor:[UIColor yellowColor]];
-    
-    for ( int i = 0; i<[arrImage count]; i++) {
-//        NSString *url=[arr objectAtIndex:i];
-//        UIButton *img=[[UIButton alloc]initWithFrame:CGRectMake(320*i, 0, 320, 189)];
-        NSString *path = [[NSBundle mainBundle] pathForResource:[arrImage objectAtIndex:i] ofType:@"jpg"];
-        UIImage *image = [UIImage imageWithContentsOfFile:path];
-//        NSLog(@"%f",image.size.width);
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
-        imageView.frame = CGRectMake(768*i, 0, 768, 1024);
-        [guideView addSubview:imageView];
-        [imageView release];
-    }
-    [arrImage release];
-    [self.view addSubview:guideView];
-    [self.view addSubview:pageControl];
-    [self.view bringSubviewToFront:startLoge];
-//	[UIView beginAnimations: nil context: nil];
-//	[UIView setAnimationDuration: 0.3f];
-//	[UIView setAnimationDelegate: self];
-//	[UIView setAnimationDidStopSelector: @selector(finishedFading)];
-//	startLoge.alpha = 1.0;
-//	[UIView commitAnimations];
-    
-    [UIView beginAnimations: nil context: nil];
-    [UIView setAnimationDuration: 0.5f];
-    [UIView setAnimationDelegate: self];
-    startLoge.alpha = 0.0;
-    [UIView setAnimationDidStopSelector:@selector(finishedFading)];
-    [UIView commitAnimations];
-    
-//    [UIView beginAnimations: nil context: nil];
-//	[UIView setAnimationDuration: 0.5f];
-//	startLoge.alpha = 0.0;
-//	[UIView commitAnimations];
-	   
-//    [self showMain];
 }
 
 - (void)finishedFading
 {
-    [self.view bringSubviewToFront:startLoge];
     [startLoge removeFromSuperview];
     [startLoge release];
 }
@@ -153,13 +159,24 @@
 
 //    //    NSLog(@"%@",self.view);
     [self.view addSubview:nav.view];
-    [self.view bringSubviewToFront:guideView];
-    [self.view bringSubviewToFront:pageControl];
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"isLogined"]) {
+        NSLog(@"isLogined = %i",[[NSUserDefaults standardUserDefaults] boolForKey:@"isLogined"]);
+        [self.view bringSubviewToFront:startLoge];
+    }else{
+                NSLog(@"isLogined = %i",[[NSUserDefaults standardUserDefaults] boolForKey:@"isLogined"]);
+        [self.view bringSubviewToFront:guideView];
+        [self.view bringSubviewToFront:pageControl];
+    }
+    
     [UIView beginAnimations: nil context: nil];
     [UIView setAnimationDuration: 0.3f];
     [UIView setAnimationDelegate: self];
-    guideView.alpha = 0.0;
-    pageControl.alpha = 0.0;
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"isLogined"]) {
+        startLoge.alpha = 0.0;
+    }else{
+        guideView.alpha = 0.0;
+        pageControl.alpha = 0.0;
+    }
     [UIView setAnimationDidStopSelector:@selector(loadMainView)];
     [UIView commitAnimations];
 
@@ -168,10 +185,18 @@
 
 - (void)loadMainView
 {
-    [guideView removeFromSuperview];
-    [guideView release];
-    [pageControl removeFromSuperview];
-    [pageControl release];
+    if ([[NSUserDefaults standardUserDefaults]boolForKey:@"isLogined"]) {
+        [startLoge removeFromSuperview];
+        [startLoge release];
+    }else{
+        [guideView removeFromSuperview];
+        [guideView release];
+        [pageControl removeFromSuperview];
+        [pageControl release];
+        [[NSUserDefaults standardUserDefaults] setBool:TRUE forKey:@"isLogined"];
+
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -185,7 +210,7 @@
     if (scrollView.contentOffset.x >= 3200) {
         [self showMain];
     }
-    pageControl.currentPage=scrollView.contentOffset.x/768;
+    pageControl.currentPage=(scrollView.contentOffset.x)/768;
 //    [self setCurrentPage:pageControl.currentPage];
 //    NSLog(@"%f",scrollView.contentOffset.x);
     
